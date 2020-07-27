3c1d2b0f08eec93d1c86d62d26f598b37239b001
Wesley Chan <wesleychan@Wesleys-MacBook-Pro.local>
2020-07-27 15:05:50 -0700

    Fixed initial App.js w/ empty Users

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,33 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  state = {
-    users: []
-  }
 
-  componentDidMount() {
-    fetch('/users')
-      .then(res => res.json())
-      .then(users => this.setState({ users }));
-  }
+export class App extends Component {
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Users</h1>
-        {this.state.users.map(user =>
-          <div key={user.id}>{user.username}</div>
-        )}
-      </div>
-    );
-  }
+    state = {
+        users: []
+    }
+
+    render() {    
+
+        return (
+            <div className="App">
+                <h1>Users</h1>
+                {this.state.users.map(user =>
+                    <div key={user.id}>{user.username}</div>
+                )}
+            </div>
+        );
+    }
+
+    componentDidMount() {
+        fetch('/users')
+            .then(res => res.json())
+            .then(users => this.setState({ users }));
+    }
+
 }
+
 
 export default App;
