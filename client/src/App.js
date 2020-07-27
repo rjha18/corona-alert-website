3680c23fa4bd20ab836a2819010339757299d981
rjha18 <rjha18@live.com>
2020-07-27 16:07:09 -0700

    Adding missing file

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { Component } from 'react';
 
-
 export class App extends Component {
-
     state = {
         users: []
     }
 
-    render() {    
+    componentDidMount() {
+        fetch('/users')
+            .then(res => res.json())
+            .then(users => this.setState({ users }));
+    }
 
+    render() {
         return (
             <div className="App">
                 <h1>Users</h1>
@@ -20,14 +22,6 @@ export class App extends Component {
             </div>
         );
     }
-
-    componentDidMount() {
-        fetch('/users')
-            .then(res => res.json())
-            .then(users => this.setState({ users }));
-    }
-
 }
-
 
 export default App;
