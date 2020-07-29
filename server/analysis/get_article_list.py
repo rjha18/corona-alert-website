f347885924a6536445f4bd18d188882f3cfa7b48
Wesley Chan <wesleychan@Wesleys-MacBook-Pro.local>
2020-07-29 16:58:51 -0700

    json

--- server/analysis/get_article_list.py
+++ server/analysis/get_article_list.py
@@ -35,12 +35,53 @@ def main():
             plain_text = ("https://www.king5.com" + text.get('href')).rstrip()
             sentence_list.append(removeUnicode(plain_text))
 
-    json_arr = json.dumps(sentence_list)
-    print(json_arr)
+
+    json_data = []
+    
+    for url in sentence_list :
+        text = getSentences(url)
+        json_data.append(text)
+
+    with open('data.json', 'w') as outfile :
+        json.dump(json_data, outfile)
+
 
 
 def removeUnicode(str) :
     return str.replace(u"\u2014", "-").replace("\u2019", "'").replace("\u2018", "'").replace("\u201c", '"').replace("\u201d", '"')
+
+def getSentences(url) :
+    buffer = BytesIO()
+    c = pycurl.Curl()
+    c.setopt(c.URL, url)
+    c.setopt(c.WRITEDATA, buffer)
+    c.perform()
+    c.close()
+
+    body = buffer.getvalue()
+    # Body is a byte string.
+    # We have to know the encoding in order to print it to a text file
+    # such as standard output.
+    html_text = body.decode("utf-8")
+
+    parsed_html = BeautifulSoup(html_text, 'html.parser')
+
+
+    sentence_list = []
+
+    for text in parsed_html.find_all('p') :
+        plain_text = text.get_text().rstrip()
+        arr = re.split(r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s', plain_text)
+        if (len(plain_text) != 0) :
+            for sentence in arr :
+                sentence_list.append(removeUnicode(sentence))
+
+    str = ""
+    for x in sentence_list :
+        str = str + x + " "
+
+    return str
+
 
 if __name__ == "__main__" :
     main()
