cbf4ab5ac666babc5b5996a47223e55ac08a71f5
edkazcarlson <ekcarlson@wpi.edu>
2020-07-29 22:58:49 -0700

    temp

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,16 +1,41 @@
 var express = require('express');
 var router = express.Router();
+const emailUser = require("../emailUser.json");
+const nodemailer = require("nodemailer");
+
+
+var mem = [{
+  email: 'sampleEmail1'
+}, {
+  email: 'sampleEmail2'
+}];
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   // res.send('respond with a resource');
-  res.json([{
-  	id: 1,
-  	username: "wchan04"
-  }, {
-  	id: 2,
-  	username: "kaz"
-  }]);
+  res.json(mem);
 });
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: emailUser
+})
+
+router.post('/', function(req,res,next){
+  mem.push(req.body);
+  res.sendStatus(200);
+  transporter.sendMail({
+    from: 'softengvvoles@gmail.com',
+    to: req.body.email,
+    subject: 'test',
+    text: 'ooga chaka'
+  }, function(error, info){
+    if (error){
+      console.log(error);
+    } else {
+      console.log('Email sent : ' + info.response);
+    }
+  }).catch();
+})
 
 module.exports = router;
